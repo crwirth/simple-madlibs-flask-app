c59e14bfef8c19422ef18c1cb19d0badb6003406
Cody Wirth <wirthrcody@gmail.com>
2020-02-18 23:12:35 -0500

    org com

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from stories import story
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "oh-so-secret"
+debug = DebugToolbarExtension(app)
+
+@app.route('/')
+def home_page():
+    prompts = story.prompts
+    return render_template("home.html", prompts=prompts)
+
+@app.route('/story')
+def story_page():
+    text = story.generate(request.args)
+    return render_template("story.html", text=text)
+
+
